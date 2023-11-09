4500b8ac61137d2c0a2735bae28b6d38ed73954e
kenan <k.qedirov199@gmail.com>
2023-11-09 14:44:48 +0400

    .

---
+++ HotelManagement/Areas/Admin/Controllers/StaffPageController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace HotelManagement.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class StaffPageController : Controller
+    {
+        private readonly IStaffService _staffManager;
+
+        public StaffPageController(IStaffService staffManager)
+        {
+            _staffManager = staffManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var staffsList = _staffManager.TGetList().Where(a=>a.StaffStatus is true).ToList();
+            return View(staffsList);
+        }
+        [HttpGet]
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddStaff(Staff staff)
+        {
+            return View();
+        }
+    }
+}
